7f05f02078a7c235f64ea9b78a7f6a9e9392bc4e
sherard-dalaguit <sherard.softwaredev@gmail.com>
2026-02-05 23:07:56 -0800

    create user model

---
+++ database/user.model.ts
@@ -0,0 +1,30 @@
+import { model, models, Schema, Document } from "mongoose";
+
+export interface IUser {
+  name: string;
+  email: string;
+  image?: string;
+  preferences: {
+    correctionIntensity: "minimal" | "moderate" | "aggressive";
+    taglishMode: boolean;
+    preferredTone?: "casual" | "polite" | "playful" | "coach"
+  }
+}
+
+export interface IUserDoc extends IUser, Document {}
+const UserSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String },
+    preferences: {
+      correctionIntensity: { type: String, enum: ["minimal", "moderate", "aggressive"], default: "moderate" },
+      taglishMode: { type: Boolean, default: false },
+      preferredTone: { type: String, enum: ["casual", "polite", "playful", "coach"], default: "casual" }
+    }
+  }
+)
+
+const User = models?.User || model<IUser>("User", UserSchema);
+
+export default User;
