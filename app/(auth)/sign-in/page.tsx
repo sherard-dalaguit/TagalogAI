5e243af81e0ee4c1b3298cf0aed868c98075b96f
sherard-dalaguit <sherard.softwaredev@gmail.com>
2026-02-07 17:31:25 -0800

    create sign-in/up form

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from "@/components/auth/auth-form";
+import React from "react";
+
+const SignInPage = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
+      <AuthForm type="sign-in" />
+    </div>
+  );
+};
+
+export default SignInPage;
